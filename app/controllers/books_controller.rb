8aea1f8802e628c14595857fb4a73682d51b454b
vagrant <vagrant@localhost.localdomain>
2020-06-11 18:23:04 +0000

    Add top_index_create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,25 @@ class BooksController < ApplicationController
   end
 
   def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to book_path(book)
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
